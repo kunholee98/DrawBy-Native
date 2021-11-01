d21afa19ef0de33cce46efd71c7d53dace2537f6
kunholee_98 <dlrjsgh750@naver.com>
2021-11-01 15:51:05 +0900

    DrawBy Native set-up

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import AppLoading from "expo-app-loading";
+// Ionicons를 사용하기 위해서는 expo font를 설치해야만 한다.
+import * as Font from "expo-font";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    return Promise.all(fontPromises);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      <Text>Hello, DrawBy!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
